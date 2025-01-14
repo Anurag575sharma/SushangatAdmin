d600995bd36dbc928558d1a74365525516d9f14b
Anurag sharma <anuragsharma575dta@gmail.com>
2025-01-14 21:23:34 +0530

    admin
    initial commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Sidebar from "./components/Sidebar/sidebar.jsx";
+import {Route, Routes} from "react-router-dom";
+import Add from "./pages/Add/Add.jsx";
+import List from "./pages/List/List.jsx";
+import Orders from "./pages/Orders/Orders.jsx";
+import {ToastContainer} from "react-toastify";
+
+const App = () => {
+  // const url = "http://localhost:4000";
+  const url = "https://sushangatmanitbackend.onrender.com";
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/List" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+        </Routes>
+      </div>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default App;
